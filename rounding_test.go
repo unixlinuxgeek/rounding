6863739a8eca7e65ab71af9bd4a53b87730abb2b
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-03-11 19:03:07 +0500

    Tests completed

--- rounding_test.go
+++ rounding_test.go
@@ -9,9 +9,40 @@ func TestRound(t *testing.T) {
 	r := 1 + rand.Float64()*(9-1) // generate random int (1 - 9)
 	t.Logf("Generate random float64 number(1-9): %f\n", r)
 
+	t.Logf("after round: %.3f", Ceil(r))
+	t.Logf("rand: %.3f", r)
+
 	if Round(r) == r {
-		t.Errorf("Test Failed: %f  is equal %f", r, Round(r))
+		t.Errorf("Test Round Failed: %f  is equal %f", r, Round(r))
 	} else {
 		t.Log("Test Round Passed!!!")
+	}
+}
+
+func TestCeil(t *testing.T) {
+	r := 1 + rand.Float64()*(9-1) // generate random int (1 - 9)
+	t.Logf("Generate random float64 number(1-9): %f\n", r)
+
+	t.Logf("after ceil: %f", Ceil(r))
+	t.Logf("rand: %f", r)
+
+	if Ceil(r) == r {
+		t.Errorf("Test Ceil Failed: %f  is equal %f", r, Ceil(r))
+	} else {
+		t.Log("Test Ceil Passed!!!")
+	}
+}
+
+func TestFloor(t *testing.T) {
+	r := 1 + rand.Float64()*(9-1) // generate random int (1 - 9)
+	t.Logf("Generate random float64 number(1-9): %f\n", r)
+
+	t.Logf("after floor: %f", Ceil(r))
+	t.Logf("rand: %f", r)
+
+	if Floor(r) == r {
+		t.Errorf("Test Floor Failed: %f  is equal %f", r, Floor(r))
+	} else {
+		t.Log("Test Floor Passed!!!")
 	}
 }
