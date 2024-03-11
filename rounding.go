e12ede6dc2024f623a5440a4e28be8b4694fd461
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-03-11 19:03:45 +0500

    Added comments

--- rounding.go
+++ rounding.go
@@ -9,10 +9,12 @@ func Round(x float64) float64 {
 	return math.Round(x*100) / 100
 }
 
+// round up
 func Ceil(x float64) float64 {
 	return math.Ceil(x*100) / 100
 }
 
+// round down
 func Floor(x float64) float64 {
 	return math.Floor(x*100) / 100
 }
